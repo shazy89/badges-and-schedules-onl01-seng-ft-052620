99355213104103661910f6bc38baa1a2b458754b
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 12:18:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,13 @@
-# Write your code here.
+
 def badge_maker(name)
 	"Hello, my name is #{name}."
 end
 
 
-# attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+#attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
+def batch_badge_creator(attendees)
+	attendees.map{|name| badge_maker(name)}
+end
+
+ #batch_badge_creator(attendees)
