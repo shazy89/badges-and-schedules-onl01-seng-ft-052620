477ed57fba06356cdcd4b34ec11bcb203887f941
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 12:50:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,8 @@ end
 
  #batch_badge_creator(attendees)
  
- def assign_rooms(attendees)
-   attendees.each.with_index { |val,index| "Hello, #{val} ! You'll be assigned to room #{index + 1}!"}
+def assign_rooms(attendees)
+   attendees.map.with_index { |val,index| "Hello, #{val} ! You'll be assigned to room #{index + 1}!"}
  end
+
+#assign_rooms(attendees)
